f1cb2689bcf60af81bef207d967b8e92512d46de
birzubir <birzubir78@gmail.com>
2024-04-29 17:21:15 +0700

    feat: setup admin layout & middleware withAuth

---
+++ src/components/views/admin/Dashboard/indexx.tsx
@@ -0,0 +1,13 @@
+import AdminLayout from "@/components/layout/AdminLayout";
+
+const DashboardAdminView = () => {
+  return (
+    <AdminLayout>
+      <div>
+        <div>Admin Page</div>
+      </div>
+    </AdminLayout>
+  );
+};
+
+export default DashboardAdminView;
---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,12 @@
+import AdminLayout from "@/components/layout/AdminLayout";
+import DashboardAdminView from "@/components/views/admin/Dashboard/indexx";
+
+const AdminPage = () => {
+  return (
+    <>
+      <DashboardAdminView />
+    </>
+  );
+};
+
+export default AdminPage;
